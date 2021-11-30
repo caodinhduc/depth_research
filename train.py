94a217db35893fad90b1e6f1afb7da4d0fd1c3dd
duccd <caodinhducuet@gmail.com>
2021-11-30 23:01:38 +0900

    fix bug

--- train.py
+++ train.py
@@ -44,7 +44,7 @@ def main_worker(gpu, ngpus_per_node, args):
         os.environ["CUDA_VISIBLE_DEVICES"]= args.gpu_num
     else:
         print("==> Single GPU Training {}".format(args.gpu))
-        # torch.cuda.set_device(args.gpu)
+        torch.cuda.set_device(args.gpu)
 
     assert torch.backends.cudnn.enabled, "Amp requires cudnn backend to be enabled."
         
@@ -94,10 +94,10 @@ def main_worker(gpu, ngpus_per_node, args):
     if (args.rank == 0):
         print("=> creating model")
     Model = LDRN(args)
-    print(torch.cuda.current_device())
 
     device = torch.device("cuda:4")
-    Model = Model.to(device)
+    print(device)
+    Model = Model.cuda()
 
     ############################### Number of model parameters ##############################
     num_params_encoder = 0
